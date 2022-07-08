b4e17ba76f7eb9f38268261cc90e51c18d6936bf
CodeDoctorDE <20452814+CodeDoctorDE@users.noreply.github.com>
2022-07-08 16:01:51 +0000

    Deploying to gh-pages from @ CodeDoctorDE/godot-stats@f37533dde142648e818107a6cac838d6d199d88a 🚀

--- _next/static/odrm61lL8EOag2ZVSXiOG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/80714a0e7ec343bb.css","static/chunks/pages/index-af6cd1a1bf32e55e.js"],"/_error":["static/chunks/pages/_error-00ad3d86f8031145.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
